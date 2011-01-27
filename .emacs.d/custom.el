7f262977c636830c290a5659e658e109eebf6482
Simon Schubert <2@0x2c.org>
2011-01-27 01:31:04 +0100

    emacs: sort emacswiki.org director at the end of load path

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -32,12 +32,16 @@
 				 'yari))
 
 ;; make all subdirs of ~/.emacs.d/vendor loadable
-
+;; however, sort the emacswiki.org directory to the end.
 (let* ((default-directory (concat dotfiles-dir "vendor")))
   (when (file-exists-p default-directory)
     (progn
       (add-to-list 'load-path default-directory)
-      (normal-top-level-add-subdirs-to-load-path))))
+      (normal-top-level-add-subdirs-to-load-path)
+      (let* ((filter '(lambda (p) (string-match "/emacswiki\\.org" p)))
+	     (tailpart (remove-if-not filter load-path)))
+	(delete-if filter load-path)
+	(mapc '(lambda (p) (add-to-list 'load-path p t)) tailpart)))))
 
 ;;; version like above, just adds directories to the end of load-path
 ;; (let* ((default-directory (concat dotfiles-dir "vendor")))
