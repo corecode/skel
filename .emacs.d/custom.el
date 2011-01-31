79ed9a2a107aa09ac1aa49072d3d5ec9e4daf5bc
Simon Schubert <2@0x2c.org>
2011-01-31 20:36:10 +0100

    emacs: hand-roll loading of vendor/ dir
    normal-top-level-add-subdirs-to-load-path records the inode of the
    loaded directories.  That means that we can't run this snippet again
    and expect new directories to be picked up.  Hand-rolling it makes it
    work as expected.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,11 +4,15 @@
   (when (file-exists-p default-directory)
     (progn
       (add-to-list 'load-path default-directory)
-      (normal-top-level-add-subdirs-to-load-path)
-      (let* ((filter '(lambda (p) (string-match "/emacswiki\\.org" p)))
-	     (tailpart (remove-if-not filter load-path)))
-	(delete-if filter load-path)
-	(mapc '(lambda (p) (add-to-list 'load-path p t)) tailpart)))))
+      (let* ((subdirs (remove-if-not 'file-directory-p (directory-files default-directory)))
+	     (filter (lambda (p) (string-match "emacswiki\\.org" p)))
+	     (frontpart (remove-if (lambda (p) (or (funcall filter p)
+					      (member p '("." "..")))) subdirs))
+	     (tailpart (remove-if-not filter subdirs)))
+	(dolist (dir frontpart)
+	  (add-to-list 'load-path (expand-file-name dir)))
+	(dolist (dir tailpart)
+	  (add-to-list 'load-path (expand-file-name dir) t))))))
 
 ;; load all other custom-*.el files
 (mapc #'load (directory-files dotfiles-dir nil "^custom-.*[.]el$"))
