7d55ec002a9ea15882d5b270adafbfafbce568d4
Simon Schubert <2@0x2c.org>
2011-01-31 20:33:05 +0100

    emacs: rename custom-packages.el, start using el-get

---
+++ .emacs.d/custom-packages.el
@@ -0,0 +1,99 @@
+;; list of packages to be installed by the starter kit.
+;; (setq starter-kit-packages (list 'kill-ring-search
+;;                                  'jump
+;;                                  'inflections
+;;                                  'findr
+;;                                  'c-eldoc
+;;                                  'css-mode
+;;                                  'guess-style
+;;                                  'htmlize
+;;                                  'http-post-simple
+;;                                  'auto-dictionary
+;;                                  'Save-visited-files
+;;                                  'smart-operator
+;;                                  'find-file-in-project
+;;                                  'gist
+;;                                  'idle-highlight
+;;                                  'inf-ruby
+;;                                  'project-local-variables
+;;                                  ))
+
+(setq starter-kit-packages (list 'kill-ring-search
+                                 'lisppaste
+                                 'xml-rpc
+                                 'org
+                                 'paredit
+                                 'jump
+                                 'inflections
+                                 'findr
+                                 'ruby-compilation
+                                 'c-eldoc
+                                 'css-mode
+                                 'guess-style
+                                 'highlight-symbol
+                                 'htmlize
+                                 'http-post-simple
+                                 'auto-dictionary
+                                 'Save-visited-files
+                                 ;; 'slime
+                                 'smart-operator
+                                 'smex
+                                 'textmate
+                                 'yasnippet-bundle
+                                 'auctex
+                                 'find-file-in-project
+                                 'gist
+                                 'idle-highlight
+                                 'inf-ruby
+                                 'magit
+                                 'project-local-variables
+                                 'ruby-mode
+                                 'yaml-mode
+                                 'yari))
+
+;; to actually install, eval
+;; (starter-kit-elpa-install)
+
+(require 'el-get)
+(setq el-get-sources
+      '(
+        ;; ac-slime
+        ;; auctex
+        ;; auto-complete
+        ;; browse-kill-ring
+        ;; emacs-goodies-el
+        ;; emacs-textmate
+        ;; flymake-point
+        ;; flymake-ruby
+        ;; grep+
+        ;; highlight-parentheses
+        ;; highlight-symbol
+        ;; hl-sexp
+        ;; magit
+        ;; markdown-mode
+        ;; multi-term
+        ;; org-mode
+        ;; package
+        ;; paredit
+        ;; pastebin
+        ;; popup-kill-ring
+        ;; python-mode
+        rinari
+        ;; ruby-compilation
+        ;; ruby-block
+        ;; ruby-end
+        ;; ruby-mode
+        ;; rvm
+        ;; session
+        ;; slime
+        ;; smart-tab
+        ;; smex
+        ;; sudo-save
+        ;; xcscope
+        ;; yari
+        ;; yaml-mode
+        ;; yasnippet
+        ))
+
+;; install with
+;; (el-get 'wait)
