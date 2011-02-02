4bdd3ab16560b9d5f868ca135efd22a4bd7e78e6
Simon Schubert <2@0x2c.org>
2011-02-02 14:58:36 +0100

    emacs: fix keybindings in multi-term

--- .emacs.d/custom-term.el
+++ .emacs.d/custom-term.el
@@ -1,22 +1,29 @@
 (require 'multi-term)
 
+(setq term-unbind-key-list
+      '("M-x"
+        "C-x"
+        "S-<left>"
+        "S-<right>"
+        "S-<up>"
+        "S-<down>"))
 ;; from multi-term.el, just use C-s and C-p with prefix.
 (setq term-bind-key-alist
       '(
         ("C-c C-c" . term-interrupt-subjob)
-        ("C-p" . previous-line)
-        ("C-n" . next-line)
+        ;; ("C-p" . previous-line)
+        ;; ("C-n" . next-line)
         ("C-c C-s" . isearch-forward)
         ("C-c C-r" . isearch-backward)
         ("C-m" . term-send-raw)
-        ("M-f" . term-send-forward-word)
-        ("M-b" . term-send-backward-word)
-        ("M-o" . term-send-backspace)
-        ("M-p" . term-send-up)
-        ("M-n" . term-send-down)
-        ("M-M" . term-send-forward-kill-word)
-        ("M-N" . term-send-backward-kill-word)
-        ("M-r" . term-send-reverse-search-history)
+        ;; ("M-f" . term-send-forward-word)
+        ;; ("M-b" . term-send-backward-word)
+        ;; ("M-o" . term-send-backspace)
+        ;; ("M-p" . term-send-up)
+        ;; ("M-n" . term-send-down)
+        ;; ("M-M" . term-send-forward-kill-word)
+        ;; ("M-N" . term-send-backward-kill-word)
+        ;; ("M-r" . term-send-reverse-search-history)
         ("M-," . term-send-input)
         ("M-." . comint-dynamic-complete)))
 
