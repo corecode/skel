3d359d2b9166cae30f3b8cc8c0b8dd0fc0d508a1
Simon Schubert <2@0x2c.org>
2011-11-22 01:13:26 +0100

    emacs: open files as root with C-x C-r

---
+++ .emacs.d/custom-80_tramp_root.el
@@ -0,0 +1,18 @@
+;;; from: http://www.emacswiki.org/emacs/TrampMode
+;;; Opens file as sudo/root on C-x C-r
+
+(require 'ido)
+
+(defun find-alternative-file-with-sudo ()
+  (interactive)
+  (let ((fname (or buffer-file-name
+                   dired-directory)))
+    (when fname
+      (if (string-match "^/sudo:root@localhost:" fname)
+          (setq fname (replace-regexp-in-string
+                       "^/sudo:root@localhost:" ""
+                       fname))
+        (setq fname (concat "/sudo:root@localhost:" fname)))
+      (find-alternate-file fname))))
+
+(global-set-key (kbd "C-x C-r") 'find-alternative-file-with-sudo)
