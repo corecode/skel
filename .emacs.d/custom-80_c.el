c9958043d01c38bc7a059cdc5f825e53850de56c
Simon Schubert <2@0x2c.org>
2012-01-09 10:06:50 +0100

    emacs: update config for cc-mode

--- .emacs.d/custom-80_c.el
+++ .emacs.d/custom-80_c.el
@@ -1,1 +1,21 @@
-(setq-default c-default-style "bsd")
+(c-add-style "2c"
+             '("bsd"
+               (c-hanging-braces-alist .
+                ((brace-list-open)
+                 (brace-entry-open)
+                 (statement-cont)
+                 (substatement-open after)
+                 (block-close . c-snug-do-while)
+                 (extern-lang-open after)
+                 (namespace-open after)
+                 (module-open after)
+                 (composition-open after)
+                 (inexpr-class-open after)
+                 (inexpr-class-close before)
+                 (class-open after))
+                )))
+
+(setq-default c-default-style
+              '((java-mode . "java")
+                (awk-mode . "awk")
+                (other . "2c")))
