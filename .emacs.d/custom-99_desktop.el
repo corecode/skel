eebfac2146a364a93d1196fbdccc1e387751c7ad
Simon Schubert <2@0x2c.org>
2012-03-18 14:13:59 +0100

    emacs/desktop: try to improve session handling

--- .emacs.d/custom-99_desktop.el
+++ .emacs.d/custom-99_desktop.el
@@ -1,6 +1,14 @@
-(require 'session)
+(require 'desktop)
 
 (setq desktop-globals-to-save '(desktop-missing-file-warning))
 (desktop-save-mode 1)
+;;; From <http://www.emacswiki.org/emacs/DeskTop>
+(defun my-desktop-autosave ()
+  (interactive)
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
+(add-hook 'auto-save-hook 'my-desktop-autosave)
 
-(add-hook 'after-init-hook 'session-initialize)
+(require 'session)
+(add-hook 'after-init-hook 'session-initialize t)
