3030ef40b4fcd6a0ea22e890eedba23a46d3fb7e
Simon Schubert <2@0x2c.org>
2012-09-17 12:43:01 +0200

    emacs/custom.el: indentation change

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -1,13 +1,14 @@
 ;; below automatically added settings by custom.
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((c-default-style . gnu) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (lexical-binding . t)))))
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-indent-next-pair-timer-interval (quote ((default 0.0005))))
+ '(safe-local-variable-values (quote ((noweb-default-code-mode . R-mode) (whitespace-line-column . 100) (c-default-style . gnu) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (lexical-binding . t)))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
