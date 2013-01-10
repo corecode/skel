975d8cc5d8e34acd0b97d3b2dfb4c532e27771d3
Simon Schubert <2@0x2c.org>
2013-01-10 14:19:38 +0100

    emacs: ido please not case sentitive

--- .emacs.d/setup-ido.el
+++ .emacs.d/setup-ido.el
@@ -4,7 +4,7 @@
 (ido-mode t)
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
-      ido-case-fold nil
+      ido-case-fold t
       ido-auto-merge-work-directories-length -1
       ido-create-new-buffer 'always
       ido-use-filename-at-point nil
