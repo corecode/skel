dce1eb1cfb663643a98c283bbc3c055a7b0e5a21
Simon Schubert <2@0x2c.org>
2013-01-16 17:04:45 +0100

    bin: add script to enable attached screens

---
+++ bin/video_switchmode.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# This script enables connected screens.
+
+. /etc/acpi/power-funcs
+
+for x in /tmp/.X11-unix/*; do
+	displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+	getXconsole;
+	if [ x"$XAUTHORITY" != x"" ]; then
+	    export DISPLAY=":$displaynum"
+	    args="--output LVDS1 --auto"
+	    xrandr_o=$(/usr/bin/xrandr -q --nograb)
+	    for out in $(echo "$xrandr_o" | awk '$2 == "disconnected" && $1 != "LVDS1" { print $1 }')
+	    do
+		args="$args --output $out --off"
+	    done
+
+	    /usr/bin/xrandr $args
+
+	    for out in $(echo "$xrandr_o" | awk '$2 == "connected" && $1 != "LVDS1" { print $1 }')
+	    do
+		args="$args --output $out --auto --right-of LVDS1"
+	    done
+
+	    /usr/bin/xrandr $args
+	fi
+done
