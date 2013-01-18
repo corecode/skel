3923804a236d40b4dce0bacda892f5eda9392922
Simon Schubert <2@0x2c.org>
2013-01-18 16:50:29 +0100

    emacs: indentation overhaul - no more auto-indent-mode

--- .emacs.d/setup-indent.el
+++ .emacs.d/setup-indent.el
@@ -1,23 +1,30 @@
-(require 'auto-indent-mode)
+;; (require 'auto-indent-mode)
 
-(setq auto-indent-untabify-on-save-file nil
-      auto-indent-blank-lines-on-move nil
-      auto-indent-kill-line-kill-region-when-active nil
-      auto-indent-next-pair nil
-      auto-indent-current-pairs nil
-      auto-indent-assign-indent-level-variables nil)
+;; (setq auto-indent-untabify-on-save-file nil
+;;       auto-indent-blank-lines-on-move nil
+;;       auto-indent-kill-line-kill-region-when-active nil
+;;       auto-indent-next-pair nil
+;;       auto-indent-current-pairs nil
+;;       auto-indent-assign-indent-level-variables nil)
 
 ;; make M-RET like RET, just without breaking the current line
 ;; (setq-default auto-indent-key-for-end-of-line-then-newline "<M-return>")
 ;; (setq-default auto-indent-key-for-end-of-line-insert-char-then-newline "<M-S-return>")
 
-(auto-indent-global-mode 1)
+;; (auto-indent-global-mode 1)
 
-;; default to indent 8, indent with tabs
+;; RET = prepare indentation
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; default to indent 8, only use tabs in certain modes
 (setq standard-indent 8
-      tab-always-indent t
+      tab-always-indent nil
       indent-tabs-mode t
       backward-delete-char-untabify-method nil)
+
+;; guess tab/offset mode
+(require 'dtrt-indent)
+(dtrt-indent-mode 1)
 
 ;; make <tab> do the right thing
 (require 'smart-tab)
