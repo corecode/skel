0d5c76d7a9fa7ab2c03337554c296a4240bd6d1c
Simon Schubert <2@0x2c.org>
2013-02-02 19:25:38 +0100

    conkeror: user-agent = firefox

--- .conkerorrc/misc.js
+++ .conkerorrc/misc.js
@@ -7,3 +7,5 @@ require("index-webjump.js");
 require("block-content-focus-change.js");
 
 require("dom-inspector.js");
+
+set_user_agent("Mozilla/6.0 (X11; U; Linux x86_64; en-US; rv:16.0.1) Gecko/20121011 Firefox/16.0.1");
