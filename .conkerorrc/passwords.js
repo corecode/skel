f34f2c14515e570e94b922bc1a78adeef61c3d1b
Simon Schubert <2@0x2c.org>
2013-02-04 21:50:01 +0100

    conkeror: use password-hasher-plus

--- .conkerorrc/passwords.js
+++ .conkerorrc/passwords.js
@@ -2,3 +2,11 @@ session_pref("signon.rememberSignons", true);
 session_pref("signon.expireMasterPassword", false);
 session_pref("signon.SignonFileName", "signons.txt");
 Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
+
+(function() {
+    var pwhp_path = "file://" + get_pref("conkeror.rcfile") + "/modules/pwhp-conk";
+    if (load_paths.indexOf(pwhp_path) == -1) {
+        load_paths.unshift(pwhp_path);
+    }
+    require("password-hasher-plus.js");
+})();
