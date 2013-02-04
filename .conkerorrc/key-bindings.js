d4bb464a78ebcb443f159d5e27f74e27e3707267
Simon Schubert <2@0x2c.org>
2013-02-04 21:49:23 +0100

    conkeror/bindings: C-l is find-url

--- .conkerorrc/key-bindings.js
+++ .conkerorrc/key-bindings.js
@@ -1,4 +1,5 @@
 define_key(content_buffer_normal_keymap, "C-t", "find-url-new-buffer");
+define_key(content_buffer_normal_keymap, "C-l", "find-url");
 //define_key(content_buffer_normal_keymap, "C-t", "make-buffer");
 define_key(content_buffer_normal_keymap, "C-w", "kill-current-buffer");
 define_key(content_buffer_normal_keymap, "M-left", "back");
