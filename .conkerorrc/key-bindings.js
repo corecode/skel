74367f85c9000045c6f2dab9b29d014cc40395d5
Simon Schubert <2@0x2c.org>
2013-05-31 12:57:04 +0200

    conkeror: don't use q for kill buffer

--- .conkerorrc/key-bindings.js
+++ .conkerorrc/key-bindings.js
@@ -1,3 +1,4 @@
+undefine_key(default_global_keymap, "q"); // just too easy to press
 define_key(content_buffer_normal_keymap, "C-t", "find-url-new-buffer");
 define_key(content_buffer_normal_keymap, "C-l", "find-url");
 //define_key(content_buffer_normal_keymap, "C-t", "make-buffer");
