2d0def8024870175dbb9fbb16912a99457aded36
Simon Schubert <2@0x2c.org>
2013-06-19 19:53:35 +0200

    emacs: yari for ruby

--- .emacs.d/setup-ruby-mode.el
+++ .emacs.d/setup-ruby-mode.el
@@ -29,5 +29,6 @@
 
 (define-key ruby-mode-map (kbd "M-l") 'insert-hashrocket)
 (define-key ruby-mode-map (kbd "C-c t") 'ruby-jump-to-other)
+(define-key ruby-mode-map (kbd "C-h r") 'yari-helm)
 
 (provide 'setup-ruby-mode)
