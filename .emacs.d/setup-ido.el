86bd27e902b47ac827050c3d9a90a8607cad0ce5
Simon Schubert <2@0x2c.org>
2013-06-19 19:51:38 +0200

    emacs/ido: for the love of god, USE THIS WINDOW

--- .emacs.d/setup-ido.el
+++ .emacs.d/setup-ido.el
@@ -7,6 +7,8 @@
       ido-case-fold t
       ido-auto-merge-work-directories-length -1
       ido-create-new-buffer 'always
+      ido-default-buffer-method 'selected-window
+      ido-default-file-method 'selected-window
       ido-use-filename-at-point nil
       ido-max-prospects 10)
 
