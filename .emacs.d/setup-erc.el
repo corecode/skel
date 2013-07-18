58a98c545cca357d45a37518ad2e626f8b8d4173
Simon Schubert <2@0x2c.org>
2013-07-18 15:51:03 +0200

    emacs/erc: set scroll-conservatively

--- .emacs.d/setup-erc.el
+++ .emacs.d/setup-erc.el
@@ -16,4 +16,17 @@
                     (when (eq major-mode 'erc-mode)
                       (setq erc-fill-column (- (window-width w) 2)))))))))
 
+(define-erc-module scroll-conservatively nil
+  "Make the prompt stay at the bottom of the window."
+  ((add-hook 'erc-mode-hook '2c-erc-set-scroll-conservatively)
+   (mapcar (lambda (buf)
+             (with-current-buffer buf
+               (2c-erc-set-scroll-conservatively)))
+           (erc-buffer-list)))
+  (t))
+
+(defun 2c-erc-set-scroll-conservatively ()
+  (make-local-variable 'scroll-conservatively)
+  (setq scroll-conservatively 5))
+
 (provide 'setup-erc)
