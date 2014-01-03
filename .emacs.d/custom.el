c73974016da089c561fcb923af700cc5cbeefa2f
Simon Schubert <2@0x2c.org>
2014-01-03 15:03:59 +0100

    emacs/custom: update settings

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,6 +9,8 @@
  '(custom-safe-themes (quote ("f641bdb1b534a06baa5e05ffdb5039fb265fde2764fbfd9a90b0d23b75f3936b" default)))
  '(doc-view-resolution 200)
  '(erc-modules (quote (completion move-to-prompt stamp spelling truncate hl-nicks netsplit fill button match track readonly networks ring autojoin noncommands irccontrols move-to-prompt menu list scroll-conservatively)))
+ '(nrepl-popup-stacktraces t)
+ '(nrepl-popup-stacktraces-in-repl t)
  '(org-agenda-files (quote ("~/.org/notes.org" "~/.org/todo.org")))
  '(org-log-into-drawer t)
  '(safe-local-variable-values (quote ((eval when (and (buffer-file-name) (file-regular-p (buffer-file-name)) (string-match-p "^[^.]" (buffer-file-name))) (emacs-lisp-mode)) (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (ada-prj-default-comp-opt . "-gnatq -gnatQ -gnat2012") (noweb-default-code-mode . R-mode) (whitespace-line-column . 100) (c-default-style . gnu) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (lexical-binding . t))))
@@ -16,7 +18,13 @@
  '(smtpmail-smtp-server "0x2c.org")
  '(smtpmail-smtp-service 587)
  '(smtpmail-smtp-user "2")
- '(smtpmail-stream-type (quote starttls)))
+ '(smtpmail-stream-type (quote starttls))
+ '(verilog-auto-endcomments nil)
+ '(verilog-auto-lineup (quote all))
+ '(verilog-auto-newline nil)
+ '(verilog-highlight-grouping-keywords t)
+ '(verilog-indent-begin-after-if nil)
+ '(verilog-indent-level-module 0))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
